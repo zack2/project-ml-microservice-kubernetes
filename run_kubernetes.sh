ef0cf7555dfd0d34a125439ee8042f89bedeed90
EC2 Default User <ec2-user@ip-172-31-10-111.ec2.internal>
2023-02-10 11:28:07 +0000

    Task 6: Deploy with Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=zackolivier/project-ml-microservice-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run project-ml-microservice-kubernetes --image=$dockerpath:latest
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward project-ml-microservice-kubernetes 8000:80
